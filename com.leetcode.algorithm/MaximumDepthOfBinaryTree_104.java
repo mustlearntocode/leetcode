512755eb175e4f49c6337b2c93bad91137abf894
Xiuyi <xiuyiye@gmail.com>
2016-07-24 14:11:38 -0400

    Problem 104

---
+++ com.leetcode.algorithm/MaximumDepthOfBinaryTree_104.java
@@ -0,0 +1,18 @@
+package com.leetcode.algorithm;
+
+/**
+ * Created by xiye on 7/24/16.
+ */
+public class MaximumDepthOfBinaryTree_104 {
+    public int maxDepth(TreeNode root) {
+        if(root==null)return 0;
+        else if(root.left!=null&&root.right!=null){
+            //this also contains the situation that root!=null&&root.left==null&&root.right==null
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) +1;
+        }else if(root.left!=null){
+            return maxDepth(root.left) +1;
+        }else{
+            return maxDepth(root.right) +1;
+        }
+    }
+}
